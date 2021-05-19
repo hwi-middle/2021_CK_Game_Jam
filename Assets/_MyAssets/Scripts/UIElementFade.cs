7fbdc1131df5e795038b175f57da3dae3063c292
hwi-middle <gnlwnd5084@gmail.com>
2021-05-19 23:43:53 +0900

    암전 Fade 연출 구현

---
+++ Assets/_MyAssets/Scripts/UIElementFade.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum EFadeType
+{
+    FadeIn,
+    FadeOut
+}
+
+public class UIElementFade : MonoBehaviour
+{
+    public EFadeType fadeType;
+    public float delay;
+    public float duration;
+    private Image imgSrc;
+
+    void Start()
+    {
+        imgSrc = GetComponent<Image>();
+        Invoke("CallFadeCoroutine", delay);
+    }
+
+    private void CallFadeCoroutine()
+    {
+        switch (fadeType)
+        {
+            case EFadeType.FadeIn:
+                StartCoroutine("FadeIn");
+                break;
+            case EFadeType.FadeOut:
+                StartCoroutine("FadeOut");
+                break;
+            default:
+                Debug.Assert(false);
+                break;
+        }
+    }
+
+    private IEnumerator FadeIn()
+    {
+        Color color = imgSrc.color;
+        while (imgSrc.color.a < 1f)
+        {
+            color.a += Time.deltaTime / duration;
+            imgSrc.color = color;
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeOut()
+    {
+        Color color = imgSrc.color;
+        while (imgSrc.color.a > 0f)
+        {
+            color.a -= Time.deltaTime / duration;
+            imgSrc.color = color;
+            yield return null;
+        }
+    }
+}
