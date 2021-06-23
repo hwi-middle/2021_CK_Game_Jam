1025f6f96021376f06ceb12a00af2eeb1c3debff
hwi-middle <gnlwnd5084@gmail.com>
2021-06-23 21:35:49 +0900

    카메라 상하회전 최대치 제한추가(Clamp Quaternion)

--- Assets/_MyAssets/Scripts/PlayerControllers/PlayerMovement.cs
+++ Assets/_MyAssets/Scripts/PlayerControllers/PlayerMovement.cs
@@ -63,16 +63,7 @@ public class PlayerMovement : MonoBehaviour
         gravity = GRAVITY_CONSTANT * gravityScale;
 
         //마우스 처리
-        float yRotation = Input.GetAxis("Mouse X") * sensitivityX;
-        float xRotation = Input.GetAxis("Mouse Y") * sensitivityY;
-
-        //X축 회전값은 -90 ~ +90도로 제한하는 Clamp 필요함
-
-        camRotation *= Quaternion.Euler(-xRotation, 0f, 0f);
-        bodyRotation *= Quaternion.Euler(0f, yRotation, 0f);
-
-        cam.transform.localRotation = camRotation;
-        transform.localRotation = bodyRotation;
+        SetCamera();
 
         //착지 상태 체크
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -184,6 +175,37 @@ public class PlayerMovement : MonoBehaviour
                 audioSource.Play();
             }
         }
+    }
+
+    void SetCamera()
+    {
+        float yRotation = Input.GetAxis("Mouse X") * sensitivityX;
+        float xRotation = Input.GetAxis("Mouse Y") * sensitivityY;
+
+        //X축 회전값은 -90 ~ +90도로 제한하는 Clamp 필요함
+
+        camRotation *= Quaternion.Euler(-xRotation, 0f, 0f);
+        camRotation = ClampRotationAroundXAxis(camRotation);
+        bodyRotation *= Quaternion.Euler(0f, yRotation, 0f);
+
+        cam.transform.localRotation = camRotation;
+        transform.localRotation = bodyRotation;
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, -90f, 90f);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
     }
 
     void CountActivatedAudioClips()
