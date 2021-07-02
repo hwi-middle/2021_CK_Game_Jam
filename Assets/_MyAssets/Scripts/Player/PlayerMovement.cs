df5f940cf192b0d653c64ff42e8c54422751d03d
hwi-middle <gnlwnd5084@gmail.com>
2021-07-02 13:08:01 +0900

    이동 관련 코드 가독성 개선

--- Assets/_MyAssets/Scripts/Player/PlayerMovement.cs
+++ Assets/_MyAssets/Scripts/Player/PlayerMovement.cs
@@ -105,11 +105,11 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //이동 관련 키 입력 받아오기
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x;
+        float z;
+        GetInputAxis(out x, out z);
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        move *= speed;
+        float y = velocity.y;
 
         //Left Shift키가 눌렸는지 확인
         bool isLeftShiftKeyDown = false;
@@ -126,7 +126,7 @@ public class PlayerMovement : MonoBehaviour
         if (canJump && Input.GetButtonDown("Jump") && isGrounded)
         {
             //설정된 높이에 맞게 점프
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            y = Mathf.Sqrt(jumpHeight * -2f * gravity);
 
             //점프키를 누르는 프레임에서 LeftShift키도 누르고 있었는지 확인
             if (!isLeftShiftKeyDown)
@@ -138,19 +138,62 @@ public class PlayerMovement : MonoBehaviour
         //Alternative Speed를 적용해야하는지 확인 후 적용
         if (shouldAlternativeSpeedApplied)
         {
-            move *= alternativeSpeedScale;
+            x *= alternativeSpeedScale;
+            z *= alternativeSpeedScale;
         }
 
-        //플레이어 xz평면 이동
-        controller.Move(move * Time.deltaTime);
+        //중력적용
+        y += gravity * Time.deltaTime;
 
-        //플레이어 y축 이동
-        velocity.y += gravity * Time.deltaTime;
+        //플레이어 이동
+        velocity = (transform.right * x * speed) + (transform.forward * z * speed) + (transform.up * y);
+        //velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+        PlayFootstepSound(isLeftShiftKeyDown, velocity);
+    }
 
+    void SetCamera()
+    {
+        float yRotation = Input.GetAxis("Mouse X") * sensitivityX;
+        float xRotation = Input.GetAxis("Mouse Y") * sensitivityY;
+
+        camRotation *= Quaternion.Euler(-xRotation, 0f, 0f);
+        camRotation = ClampRotationAroundXAxis(camRotation);
+        bodyRotation *= Quaternion.Euler(0f, yRotation, 0f);
+
+        cam.transform.localRotation = camRotation;
+        transform.localRotation = bodyRotation;
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, -90f, 90f);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+
+    void GetInputAxis(out float x, out float z)
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+    }
+
+    void PlayFootstepSound(bool isLeftShiftKeyDown, Vector3 v)
+    {
         //발걸음 소리 출력이 필요한지 확인
         time += Time.deltaTime;
         bool isFootstepSoundRequired;
+
+        //클립 개수 확인
         CountActivatedAudioClips();
         int activatedAudioClipsNum = activatedAudioDatas.Count;
 
@@ -159,6 +202,7 @@ public class PlayerMovement : MonoBehaviour
         {
             isFootstepSoundRequired = false;
         }
+
         //Left Shift키가 눌렸을 때에는 alternativeSpeedScale값에 따라 frequency가 조절
         else if (isLeftShiftKeyDown)
         {
@@ -168,7 +212,7 @@ public class PlayerMovement : MonoBehaviour
             if (hasStamina)
             {
                 currentStamina -= staminaDecreasementAmount * Time.deltaTime;
-                if(currentStamina < 0)
+                if (currentStamina < 0)
                 {
                     currentStamina = 0;
                 }
@@ -181,7 +225,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //움직이고 있는지 확인
-        if (move.x != 0 || move.z != 0) isMoving = true;
+        if (v.x != 0 || v.z != 0) isMoving = true;
         else isMoving = false;
 
         //발걸음 소리 출력
@@ -211,37 +255,6 @@ public class PlayerMovement : MonoBehaviour
                 audioSource.Play();
             }
         }
-    }
-
-    void SetCamera()
-    {
-        float yRotation = Input.GetAxis("Mouse X") * sensitivityX;
-        float xRotation = Input.GetAxis("Mouse Y") * sensitivityY;
-
-        //X축 회전값은 -90 ~ +90도로 제한하는 Clamp 필요함
-
-        camRotation *= Quaternion.Euler(-xRotation, 0f, 0f);
-        camRotation = ClampRotationAroundXAxis(camRotation);
-        bodyRotation *= Quaternion.Euler(0f, yRotation, 0f);
-
-        cam.transform.localRotation = camRotation;
-        transform.localRotation = bodyRotation;
-    }
-
-    Quaternion ClampRotationAroundXAxis(Quaternion q)
-    {
-        q.x /= q.w;
-        q.y /= q.w;
-        q.z /= q.w;
-        q.w = 1.0f;
-
-        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
-
-        angleX = Mathf.Clamp(angleX, -90f, 90f);
-
-        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
-
-        return q;
     }
 
     void CountActivatedAudioClips()
