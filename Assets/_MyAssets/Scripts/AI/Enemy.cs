e8f851c912f3c6f8cda923e3694b7655bbd984d5
hwi-middle <gnlwnd5084@gmail.com>
2021-07-06 13:28:40 +0900

    적 AI 형식을 2개로 나누기 위한 열거형 추가 및 데모 씬 조정

--- Assets/_MyAssets/Scripts/AI/Enemy.cs
+++ Assets/_MyAssets/Scripts/AI/Enemy.cs
@@ -7,15 +7,34 @@ public class Enemy : MonoBehaviour
 {
     NavMeshAgent agent;
 
-    public Transform target;
+    [SerializeField] private EEnemyType type;
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform origin;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        if(type == EEnemyType.Main)
+        {
+            StartChasing();
+        }
     }
 
     void Update()
     {
         agent.SetDestination(target.position);
+    }
+
+
+    //Sub Enemy일 경우 외부 Trigger에서 호출
+    public void StopAndReturnToOrigin()
+    {
+
+    }
+
+    public void StartChasing()
+    {
+
     }
 }
