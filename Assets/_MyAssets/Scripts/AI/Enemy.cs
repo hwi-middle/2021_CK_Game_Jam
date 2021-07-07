cac9cfff575311d565fe3c105747b3c0953d4e9c
hwi-middle <gnlwnd5084@gmail.com>
2021-07-07 19:39:20 +0900

    Enemy 스크립트에서 작업의 우선순위 시스템을 적용하여 활동범위 경계에서 적이 오락가락하지 않도록 수정

--- Assets/_MyAssets/Scripts/AI/Enemy.cs
+++ Assets/_MyAssets/Scripts/AI/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    NavMeshAgent agent;
+    private NavMeshAgent agent;
 
     [SerializeField] private EEnemyType type;
     [SerializeField] private Transform origin;
@@ -15,8 +15,14 @@ public class Enemy : MonoBehaviour
 
     public EEnemyType Type { get { return type; } }
 
+    //우선순위는 숫자가 작을수록 높음
+    //우선순위 충돌시 새로운 작업 우선
+    private int currentTaskPriority;
+    private const int MAX_PRIORITY = 10000; 
+
     void Start()
     {
+        currentTaskPriority = MAX_PRIORITY;
         agent = GetComponent<NavMeshAgent>();
         player = PlayerMovement.Instance.transform;
 
@@ -37,22 +43,60 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        agent.SetDestination(currentTarget.position);
+            agent.SetDestination(currentTarget.position);
+
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    currentTaskPriority = MAX_PRIORITY;
+                    Debug.Log("목적지 도착: " + currentTarget.name);
+                }
+            }
+        }
     }
 
 
     //Sub Enemy일 경우 외부 Trigger에서 호출
+    //우선순위 높음
     public void StopAndReturnToOrigin()
     {
+        const int PRIORITY = 1;
+
+        //우선순위가 더 높은 작업이 진행 중일 경우 스킵
+        if(ComparePriority(PRIORITY, currentTaskPriority) > 0)
+        {
+            return;
+        }
+
+        currentTaskPriority = PRIORITY;
         currentTarget = origin;
     }
 
     public void StartChasing()
     {
+        const int PRIORITY = 2;
+
+        //우선순위가 더 높은 작업이 진행 중일 경우 스킵
+        if (ComparePriority(PRIORITY, currentTaskPriority) > 0)
+        {
+            return;
+        }
+
+        currentTaskPriority = PRIORITY;
         currentTarget = player;
     }
 
-    private void OnTriggerEnter(Collider other)
+    //C의 strcmp처럼 단순 뺄셈처리
+    //결과값이 음수면 첫 번째 인자의 우선순위가 더 높은 것
+    public int ComparePriority(int a, int b)
+    {
+        return a - b;
+    }
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
