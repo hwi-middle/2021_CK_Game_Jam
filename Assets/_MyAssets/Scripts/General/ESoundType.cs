0d6cb9bf32b88aaf31ecbc35d21f84a4cce406bb
hwi-middle <gnlwnd5084@gmail.com>
2021-07-20 18:12:31 +0900

    볼륨조절 구현

---
+++ Assets/_MyAssets/Scripts/General/ESoundType.cs
@@ -0,0 +1,6 @@
+public enum ESoundType
+{
+    BGM,
+    SE,
+    Footstep
+}
---
+++ Assets/_MyAssets/Scripts/SoundVolumeModifier.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundVolumeModifier : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public ESoundType type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (type)
+        {
+            case ESoundType.BGM:
+                audioSource.volume = PlayerPrefs.GetFloat("BGMValue", 1f);
+                break;
+            case ESoundType.SE:
+                audioSource.volume = PlayerPrefs.GetFloat("SEValue", 1f);
+                break;
+            case ESoundType.Footstep:
+                audioSource.volume = PlayerPrefs.GetFloat("FootstepSoundValue", 1f);
+                break;
+            default:
+                Debug.Assert(false);
+                break;
+        }
+    }
+}
