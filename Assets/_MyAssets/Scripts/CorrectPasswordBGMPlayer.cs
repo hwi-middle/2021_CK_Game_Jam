9cb902bf9007399dfb26e713c759254690ece632
Ju Hwijung <gnlwnd5084@gmail.com>
2021-07-29 01:47:43 +0900

    접근성 설정에서 특수효과를 끄면 엔딩씬에서 글리치가 적용되지 않도록 수정

--- Assets/_MyAssets/Scripts/CorrectPasswordBGMPlayer.cs
+++ Assets/_MyAssets/Scripts/CorrectPasswordBGMPlayer.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class CorrectPasswordBGMPlayer : MonoBehaviour
 {
     private PlayerMovement player;
     private AudioSource audioSource;
+    [SerializeField] Volume volume;
+    private LimitlessGlitch8 glitch1;
+    private LimitlessGlitch12 glitch2;
     [SerializeField] private AudioClip endHitClip;
     [SerializeField] private Image cover;
 
     // Start is called before the first frame update
     void Start()
     {
+        volume.profile.TryGet(out glitch1);
+        volume.profile.TryGet(out glitch2);
+
         player = PlayerMovement.Instance;
         audioSource = GetComponent<AudioSource>();
         StartCoroutine(ControlMusicAndScene());
+
+        if (PlayerPrefs.GetInt("DisableVFX") == 1)
+        {
+            glitch1.enable.value = false;
+            glitch2.enable.value = false;
+        }
+
     }
 
     // Update is called once per frame
